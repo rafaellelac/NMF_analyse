bcafa8911bb513c1cb4cde2b716744dbdef2562e
Rafaelle Lacraz <rafaelle.lacraz@gmail.com>
2025-06-18 16:29:45 +0200

    ok

--- app.py
+++ app.py
@@ -219,5 +219,6 @@ def afficher_analyse(match_file, equipe, type_analyse, joueurs_collectif, joueur
     return fig
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 8050))
+    app.run(host="0.0.0.0", port=port, debug=True)
 
